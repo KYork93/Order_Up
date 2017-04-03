369da83b95b9e976bc6163cf1ade955ace5d6b33
Kyork93 <kyorkston@gmail.com>
2017-04-03 16:39:57 +0100

    add seed data

---
+++ dbseeds.js
@@ -0,0 +1,29 @@
+use drink_app;
+
+db.pubs.insert({
+      id:1,
+      pubName: 'Lebowskis',
+      pubSrc: 'http://farm7.static.flickr.com/6209/6128315560_f2fd0abf4a.jpg',
+      drinksMenu: [{
+        id:1,
+        drinkName: 'White Russian',
+        drinkSrc: 'http://www.kahlua.com/globalassets/classic-cocktails/white-russian/kahlua-white-russian-highlighted.png/CocktailHighlight',
+        price: 6.99},
+      {
+        id: 2,
+        drinkName: 'Budweiser',
+        drinkSrc: 'https://s3-eu-central-1.amazonaws.com/centaur-wp/creativereview/prod/content/uploads/2016/01/2011budweiser-can.jpg',
+        drinkPrice: 4.50
+      }]
+    },
+    {
+      id: 2,
+      pubName: 'Paradise Palms',
+      pubSrc: 'http://files.stv.tv/imagebase/300/605x454/300373-paradise-palms-lothian-street.jpg',
+      drinksMenu: [{
+        id:1,
+        drinkName: 'Buckfast Daiquiri',
+        drinkSrc: 'https://pbs.twimg.com/media/CIljQtVWcAA-PMg.jpg'
+      }]
+    }
+)
