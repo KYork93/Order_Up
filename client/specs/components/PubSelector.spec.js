6a737362f6cb325d152536a87ab453d5cadcdeb0
Kyork93 <kyorkston@gmail.com>
2017-04-04 12:52:45 +0100

    add render spec in MainCOntainer

---
+++ client/specs/components/PubSelector.spec.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import { expect } from 'chai'
+// import { mount, shallow, render } from 'enzyme'
+//
+// import PubSelector from '../../src/components/PubSelector'
+//
+// describe('Pub Selector', () => {
+//
+//   it('should a select element', () => {
+//     const wrapper = mount(<PubSelector />)
+//     expect(wrapper.state('selectedIndex')).to.equal('undefined')
+//   })
+//
+// })
---
+++ client/specs/containers/MainContainer.spec.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { expect } from 'chai'
+import { mount, shallow, render } from 'enzyme'
+
+import MainContainer from '../../src/containers/MainContainer'
+import CurrentRound from '../../src/components/CurrentRound'
+import PubSelector from '../../src/components/PubSelector'
+import PubDetail from '../../src/components/PubDetail'
+import DrinkDetails from '../../src/components/DrinkDetails'
+
+describe('Main Container', () => {
+
+  it('should render its components', () => {
+    const wrapper = mount(<MainContainer />)
+    expect(wrapper.containsAllMatchingElements([
+      <CurrentRound />,
+      <PubSelector />,
+      <PubDetail />,
+      <DrinkDetails />
+    ])).to.equal(true)
+  })
+
+  it('should have state', () => {
+    const wrapper = mount(<MainContainer />)
+    expect(wrapper.state('focusPub')).to.equal(null)
+    // empty array object
+    expect(wrapper.state('drinksRound').length).to.equal(0)
+  })
+
+})
