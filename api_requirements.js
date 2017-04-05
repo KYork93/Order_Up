823bf52112e9b300844d4fdfa3a56571bd8ccdc2
Kyork93 <kyorkston@gmail.com>
2017-04-05 14:02:33 +0100

    add api requirement

---
+++ api_requirements.js
@@ -0,0 +1,40 @@
+Bars:
+[
+  {
+    "id": "1",
+    "pubName": 'Lebowskis',
+    "lat": "53.1",
+    "lng": "-3.1",
+    "pubSrc": 'http://farm7.static.flickr.com/6209/6128315560_f2fd0abf4a.jpg',
+    "drinksMenu": [{
+      "id":" 1",
+      "drinkName": 'White Russian',
+      "drinkSrc": 'http://www.kahlua.com/globalassets/classic-cocktails/white-russian/kahlua-white-russian-highlighted.png/CocktailHighlight',
+      "price": "6.99"
+    },
+    {
+      "id": "2",
+      "drinkName": 'Budweiser',
+      "drinkSrc": 'https://s3-eu-central-1.amazonaws.com/centaur-wp/creativereview/prod/content/uploads/2016/01/2011budweiser-can.jpg',
+      "price": "4.49"
+    }]
+  }
+]
+
+Rounds:
+[
+  {
+    "id": "1",
+    "pub_id":" 1",
+    "ordered_at": "Tue Apr 04 2017 21:46:54 GMT+0100",
+    "drinks_ordered": [
+      {
+        "drink_id": "2"
+      },
+      {
+        "drink_id": "1"
+      }
+    ],
+    "total_price": "11.48"
+  }
+]
